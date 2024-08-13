7eff6497adbcabe885273205f818e5bc54deb64c
Durgeshityar <dchandrakar2909@gmail.com>
2024-08-13 16:27:05 +0530

    fixed dashboard UI

--- src/app/(dashboard)/banner.tsx
+++ src/app/(dashboard)/banner.tsx
@@ -26,17 +26,17 @@ export const Banner = () => {
     )
   }
   return (
-    <div className=" text-white aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
-      <div className="rounded-full size-28  items-center justify-center bg-white/50 hidden md:flex">
-        <div className="rounded-full size-20 flex items-center justify-center bg-white">
-          <Sparkles className="h-20 text-[#0073ff] fill-[#0073ff]" />
+    <div className="text-white min-h-[150px] flex gap-x-4 p-4 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
+      <div className="rounded-full size-24 items-center justify-center bg-white/50 hidden md:flex">
+        <div className="rounded-full size-16 flex items-center justify-center bg-white">
+          <Sparkles className="h-16 text-[#0073ff] fill-[#0073ff]" />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2">
-        <h1 className=" text-2xl md:text-3xl font-semibold">
+      <div className="flex flex-col gap-y-1">
+        <h1 className="text-xl md:text-3xl font-semibold">
           Visualize your ideas with Canvas AI
         </h1>
-        <p className="  text-xs md:text-sm mb-2">
+        <p className="text-sm md:text-base mb-2">
           Turn your inspirations into stunning designs in no time. Simply upload
           an image, and let our AI do the rest. Create professional-grade
           visuals effortlessly!
@@ -45,7 +45,7 @@ export const Banner = () => {
           disabled={mutation.isPending}
           onClick={onClick}
           variant={'secondary'}
-          className="w-[160px]"
+          className="w-full md:w-[160px]"
         >
           Start Creating
           <ArrowRight className="size-4 ml-2" />
